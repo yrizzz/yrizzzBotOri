a9e0cdc8694aba9ec1828b3ee75a3ff222a2cd72
riezz <rekberariez@gmail.com>
2025-07-11 16:14:12 +0700

    push

--- Commands/socialmedia/igprofile.js
+++ Commands/socialmedia/igprofile.js
@@ -9,9 +9,9 @@ export default {
             await ctx.react('⏳');
             let data = helper.filtermessage(await ctx.msg, ...await ctx.args);
 
-            const result = await req('GET', `https://yrizzz.my.id/api/v1/socialmedia/igprofile?username=${data}`);
-
-            if (result.status) {
+            const result = await req('GET', `https://yrizzz.my.id/api/v1/socialmedia/igprofile?username=jokowi`);
+            console.log(result)
+            if (result.status == 200) {
                 let caption = '✅ Success\n\n';
                 caption += 'Fullname : ' + result.data.full_name + '\n';
                 caption += 'Username : ' + result.data.username + '\n';
@@ -20,12 +20,13 @@ export default {
                 caption += 'Following : ' + result.data.following_count + '\n';
                 caption += 'Total post : ' + result.data.media_count;
 
-                await ctx.reply([{ image: { url: result.data.hd_profile_pic_url_info.url }, caption: caption }, { ephemeralExpiration: m?.message?.extendedTextMessage?.contextInfo?.expiration ?? 0 }]);
+                await ctx.reply([{ image: { url: result.data.hd_profile_pic_url_info.url }, caption: caption }, { ephemeralExpiration: ctx?.msg?.message?.extendedTextMessage?.contextInfo?.expiration ?? 0 }]);
                 await ctx.react('✅');
             }
             return;
 
         } catch (err) {
+            console.log(err)
             await ctx.react('⛔');
             await ctx.reply('Failed to fetch please contact the owner');
 
--- Handlers/req.js
+++ Handlers/req.js
@@ -1,15 +1,23 @@
 import axios from 'axios';
 
-export default async function req(method, url, formdata = null, header = null) {
-    const option = {
-        url,
-        method,
-        maxBodyLength: Infinity,
-        headers: header,
-        data: formdata
-    };
+export default async function req(method, url, formdata = null, headers = {}) {
+    try {
+        const response = await axios.request({
+            method,
+            url,
+            timeout: 3000,
+            maxBodyLength: Infinity,
+            headers,
+            data: formdata
+        });
 
-    return await axios.request(option)
-        .then(response => response.data)
-        .catch(err => err);
+        return response;
+    } catch (error) {
+        return {
+            success: false,
+            message: error?.response?.data?.message || error.message,
+            status: error?.response?.status || 500,
+            error: error?.response?.data || null
+        };
+    }
 }
